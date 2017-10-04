e6602f73ffaadf7f7e72f34aa59548d1c432cf54
Matthew Elmquist <mattelmquist@gmail.com>
2017-10-04 09:55:01 -0400

    first round of tests for TDD

--- src/tests/first.test.js
+++ src/tests/first.test.js
@@ -2,7 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
 
-it('renders without crashing POO', () => {
+it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+it('accepts valid URLs into the main text field', () => {
+  return false;
+})
+
+
+it('messages the user when an invalid URL is given', () => {
+  return false;
+})
+
+it('makes an HTTP request when a user submits a valid link to crawl on', () => {
+  return false;
+})
