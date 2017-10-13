457d09fa58a08d82d455f05c83667cc3d66cb012
Matthew Elmquist <mattelmquist@gmail.com>
2017-10-12 20:41:27 -0400

    new root component

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import '../assets/main/main.css';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {};
+    }
+
+
+    render() {
+        return (
+            <div className="App-Background">
+                I am the new app component
+
+
+            </div>
+        )
+    }
+}
+
+export default App;
